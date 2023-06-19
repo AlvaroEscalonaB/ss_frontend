b50d51a616f2df9253acc5940c0e5042c0a3d1cc
alvaro14s <escalona.alvaro.13@gmail.com>
2023-06-18 20:58:59 -0400

    12 Refactor navbar in Root with a minimal style and effect to mark the current screen

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,31 +1,51 @@
 import { useEffect } from "react";
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, Link, useNavigate, useLocation } from "react-router-dom";
 import { useUserStore } from "./store/userStore";
+
+interface NavbarRoute {
+  route: String,
+  labelRoute: String,
+}
 
 const Root = () => {
 
+  const location = useLocation();
   const navigate = useNavigate();
   const user = useUserStore(state => state.user);
+
+  const navbarRoutes: NavbarRoute[] = [
+    {
+      route: "/cat-facts",
+      labelRoute: "Cat Fact",
+    },
+    {
+      route: "/user-favorites",
+      labelRoute: "Your Favorites",
+    },
+    {
+      route: "/community-favorites",
+      labelRoute: "Community",
+    }
+  ];
+
   useEffect(() => {
-    if (!user.token && !user.username) {
+    if (user.token && user.username) {
       navigate("/login")
     }
-  })
-
+  });
+  
   return (
     <main>
-      <section className="flex flex-row">
-        <div>
-          <Link to="cat-facts"> cat facts </Link>
-        </div>
-        <div>
-          <Link to="user-favorites"> Your favorites </Link>
-        </div>
-        <div>
-          <Link to="community-favorites"> Community Favorites </Link>
-        </div>
-      </section>
-      <section>
+      <nav className="flex flex-row gap-x-4 bg-teal-800 text-white px-12">
+        { navbarRoutes.map( ({route, labelRoute}) => (
+          <div key={route as any} className={`py-2 px-2 ${ route === location.pathname && "border-b-2 border-white"}`}>
+            <Link to={route as any}> 
+              {labelRoute} 
+            </Link>  
+          </div>
+        ))}
+      </nav>
+      <section className="p-10">
         <Outlet />
       </section> 
     </main>
