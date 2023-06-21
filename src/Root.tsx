0c21d36b08fdc462ae88f29db682625d5a3bbf14
alvaro14s <escalona.alvaro.13@gmail.com>
2023-06-21 15:57:43 -0400

    33 logout on Root page

--- src/Root.tsx
+++ src/Root.tsx
@@ -25,27 +25,42 @@ const navbarRoutes: NavbarRoute[] = [
 const Root = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const user = useUserStore(state => state.user);
+  const userStore = useUserStore();
+
+  const handleLogout = () => {
+    userStore.logoutUser();
+    navigate('/login');
+  }
 
   useEffect(() => {
     if (location.pathname === "/") {
       navigate("/cat-facts");
     }
-    if (!user.token && !user.username) {
+    if (!userStore.user.token) {
       navigate("/login");
     }
-  });
+  }, []);
   
   return (
     <main className="min-h-screen flex flex-col">
-      <nav className="flex flex-row gap-x-4 bg-teal-800 text-white px-12">
-        { navbarRoutes.map( ({route, labelRoute}) => (
-          <div key={route as any} className={`py-3 px-2 ${ route === location.pathname && "border-b-4 border-white"}`}>
-            <Link to={route as any}>
-              {labelRoute} 
-            </Link>  
-          </div>
-        ))}
+      <nav className="flex flex-row justify-between bg-teal-800 text-white px-12">
+        <div className="flex flex-row">
+          { navbarRoutes.map( ({route, labelRoute}) => (
+            <div key={route as any} className={`py-3 px-2 ${ route === location.pathname && "border-b-4 border-white"}`}>
+              <Link to={route as any}>
+                {labelRoute} 
+              </Link>  
+            </div>
+          ))}
+        </div>
+        <div>
+          <button 
+            onClick={handleLogout}
+            className="py-3 px-4 hover:bg-teal-700 hover:border-b-4 hover:border-b-teal-700"
+          >
+            Log Out
+          </button>
+        </div>
       </nav>
       <section className="p-10 flex-1">
         <Outlet />
