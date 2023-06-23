ee2cbd58b2e4d4c471d5818b8baa1345b57c701c
Alvaro Escalona <ajescalona@uc.cl>
2023-06-23 04:04:16 -0400

    Change the root for bug

--- src/router.tsx
+++ src/router.tsx
@@ -8,34 +8,37 @@ import SignUp from "./screens/SignUp";
 import CommunityFavorites from "./screens/CommunityFavorites";
 import CatFact from "./screens/CatFact";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorNotFound />,
-    children: [
-      {
-        path: "user-favorites",
-        element: <UserFavorites />,
-      },
-      {
-        path: "community-favorites",
-        element: <CommunityFavorites />
-      },
-      {
-        path: "cat-facts",
-        element: <CatFact />
-      }
-    ]
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorNotFound />,
+      children: [
+        {
+          path: "user-favorites",
+          element: <UserFavorites />,
+        },
+        {
+          path: "community-favorites",
+          element: <CommunityFavorites />,
+        },
+        {
+          path: "cat-facts",
+          element: <CatFact />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp />,
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/ss_frontend/" }
+);
 
 export default router;
